d1295a04a1ce4088486156469d0942a621ed726d
mie_gerald <romashkoviy.chay@gmail.com>
2019-11-12 21:56:32 +0400

    This code prints all prime numbers between two inputs

--- main.js
+++ main.js
@@ -2,13 +2,12 @@ const util = require("./myUtils.js");
 
 const reader = require("readline-sync");
 
-const num1 = reader.question('Input a positive number. ');
-const num2 = reader.question('Input another positive number. ');
+const num1 = reader.question('Input a positive integer. ');
+const num2 = reader.question('Input another positive integer. ');
 
 if (isNaN(num1) || isNaN(num2) || num1 < 0 || num2 < 0) {
 
-    console.log("At least one of your inputs is not a positive number.");
-
+    console.log("At least one of your inputs is not a positive integer.");
 }
 
 
@@ -17,6 +16,5 @@ const range = [];
 for (let i = Math.min(num1,num2); i <= Math.max(num1,num2); i++) {
 
    if(util.isPrime(i)) range.push(i);
-
 }
 util.print(range);
